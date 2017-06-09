1c6b4fe722ca2475c4efbe7182e2a1d0a22e2a43
Harith Neralla <hneralla56@gmail.com>
2017-06-08 17:01:47 -0700

    getWsOps(...) should process param/return types

--- WsOperations/Service1.svc.cs
+++ WsOperations/Service1.svc.cs
@@ -9,34 +9,71 @@ using System.Xml;
 
 namespace WsOperations
 {
-    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "Service1" in code, svc and config file together.
-    // NOTE: In order to launch WCF Test Client for testing this service, please select Service1.svc or Service1.svc.cs at the Solution Explorer and start debugging.
+    // Author: Harith Neralla
+    // CSE 445 Assignment 3: Part 1
+
     public class Service1 : IService1
     {
-       public string[] getWsOperations(string url)
+        // Service 7: WsOperations
+        // Description: Analyze a WSDL file in XML format and return operation names and their
+        // corresponding input parameter and return types.
+
+        public string[] getWsOperations(string url)
         {
-           
-            XmlDocument xd = new XmlDocument();
+            
+            XmlDocument xd = new XmlDocument(); 
+            string[] WsOpDetails;
+            int opIndex = 0; // to iterate through WsOpDetails
 
             try
             {
-                xd.Load(url);
+                xd.Load(url); 
             }
-            catch (XmlException xe) 
+            catch (XmlException xe)   
             {
                 return new[] { xe.ToString() }; // converts string to string[]
             }
 
-            XmlNodeList portNodes = xd.SelectSingleNode("wsdl:definitions").SelectNodes("wsdl:portType");
+            XmlNode defNode = xd.SelectSingleNode("wsdl:definitions");
+            XmlNodeList opNodes = defNode.SelectSingleNode("wsdl:portType").SelectNodes("wsdl:operation");
+            XmlNodeList dataTypes = defNode.SelectSingleNode("wsdl:types").SelectSingleNode("xs:schema").SelectNodes("xs:element");
 
-            string[] WsOpDetails = new string[portNodes.Count]; // initializes array with the number of operations (ports) 
+            WsOpDetails = new string[opNodes.Count]; // initializes array with the number of operations (ports) 
 
-            foreach (XmlNode node in portNodes)
+            XmlNode elementNode = dataTypes.Item(0); 
+
+            foreach (XmlNode node in opNodes)
             {
-                string opName = node.SelectSingleNode("wsdl:operation").Attributes.GetNamedItem("name").Value; // obtains the operation name  
-                //string paramTypes =  
+                string paramTypes = "";
+                string returnType = "";
+                string opName = node.Attributes.GetNamedItem("name").Value; // obtains the operation name
+                               
+                for (int index = 0; index < 2; index++)
+                {
+                    if (elementNode.Attributes.GetNamedItem("name").Value == opName)
+                    {
+                        XmlNodeList list = elementNode.SelectSingleNode("xs:complexType")
+                                            .SelectSingleNode("xs:sequence").SelectNodes("xs:element"); // list of element nodes (parameters)
+                        
+                        foreach (XmlNode elNode in list) // iterates through n number of parameters
+                        {
+                            paramTypes += elNode.Attributes.GetNamedItem("type").Value + "-"; // builds a string of parameter types separated by "-"
+                        }
+
+                    }
+
+                    if (elementNode.Attributes.GetNamedItem("name").Value == (opName + "Response"))
+                    {
+                        returnType = elementNode.SelectSingleNode("xs:complexType").SelectSingleNode("xs:sequence") // obtains return type 
+                                      .SelectSingleNode("xs:element").Attributes.GetNamedItem("type").Value; 
+                    }
+
+                    elementNode = elementNode.NextSibling; // goes to the next elementNode
+                }
+
+                WsOpDetails[opIndex] = returnType + "||" + opName + "||" + paramTypes; // inserts data into the array
+                opIndex++; 
             }
-            
 
             return WsOpDetails;
         }
