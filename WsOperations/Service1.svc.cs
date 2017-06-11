5696c0dcf32559fc879ac37fef56d8a5a6f37f39
Harith Neralla <hneralla56@gmail.com>
2017-06-10 21:44:26 -0700

    ignore

--- WsOperations/Service1.svc.cs
+++ WsOperations/Service1.svc.cs
@@ -1,4 +1,8 @@
 ﻿using System.Xml;
+using System.Security.Cryptography;
+using System.Web.Services.Description;
+using System.Xml.Schema;
+using System.Collections.Generic;
 
 namespace WsOperations
 {
@@ -13,66 +17,109 @@ namespace WsOperations
 
         public string[] getWsOperations(string url)
         {
-            XmlDocument xd = new XmlDocument(); 
-            string[] WsOpDetails;
-            int opIndex = 0; // to iterate through WsOpDetails
+            List<string> WsOpDetails = new List<string>();
+            XmlTextReader xd = new XmlTextReader(url);
 
-            try
+            ServiceDescription myDescription = ServiceDescription.Read(xd);
+            Types dataTypes = myDescription.Types;
+            XmlSchema schemaElement = dataTypes.Schemas[0];
+            PortTypeCollection portTypes = myDescription.PortTypes;
+
+            string serviceName = myDescription.Services[0].Name;
+
+            foreach(PortType type in portTypes)
             {
-                xd.Load(url); 
-            }
-            catch (XmlException xe)   
-            {
-                return new[] { xe.ToString() }; // converts string to string[]
-            }
-
-            XmlNamespaceManager nsmgr = new XmlNamespaceManager(xd.NameTable);
-            nsmgr.AddNamespace("wsdl", "http://schemas.xmlsoap.org/wsdl/"); // is this the solution?? 
-
-            XmlNode defNode = xd.SelectSingleNode("definitions");
-
-
-            XmlNodeList opNodes = defNode.SelectSingleNode("wsdl:portType", nsmgr).SelectNodes("operation");
-            XmlNodeList dataTypes = defNode.SelectSingleNode("types", nsmgr).SelectSingleNode("schema", nsmgr).SelectNodes("element");
-
-            WsOpDetails = new string[opNodes.Count]; // initializes array with the number of operations (ports) 
-
-            XmlNode elementNode = dataTypes.Item(0); 
-
-            foreach (XmlNode node in opNodes)
-            {
-                string paramTypes = "";
-                string returnType = "";
-                string opName = node.Attributes.GetNamedItem("name").Value; // obtains the operation name
-                               
-                for (int index = 0; index < 2; index++)
+                foreach(Operation op in type.Operations)
                 {
-                    if (elementNode.Attributes.GetNamedItem("name").Value == opName)
-                    {
-                        XmlNodeList list = elementNode.SelectSingleNode("xs:complexType") // list of element nodes (parameters)
-                                            .SelectSingleNode("xs:sequence").SelectNodes("xs:element"); 
-                        
-                        foreach (XmlNode elNode in list) // iterates through n number of parameters
-                        {
-                            paramTypes += elNode.Attributes.GetNamedItem("type").Value + "-"; // builds a string of parameter types separated by "-"
-                        }
+                    string opName = op.Name;
+                    string inMessage = op.Messages.Input.Message.Name;
+                    string outMessage = op.Messages.Output.Message.Name;
 
+                    string inMessagePart = myDescription.Messages[inMessage].Parts[0].Element.Name;
+                    string outMessagePart = myDescription.Messages[outMessage].Parts[0].Element.Name;
+
+                    Parameter[] inputParameters = GetParameters(myDescription, inMessagePart);
+                    Parameter[] outputParameters = GetParameters(myDescription, outMessagePart);
+
+                    string inputParams = "";
+                    string outParams = "";
+
+                    foreach(Parameter param in inputParameters)
+                    {
+                        inputParams += param.Type + "/";
                     }
 
-                    if (elementNode.Attributes.GetNamedItem("name").Value == (opName + "Response"))
+                    foreach(Parameter param in outputParameters)
                     {
-                        returnType = elementNode.SelectSingleNode("xs:complexType").SelectSingleNode("xs:sequence") // obtains return type 
-                                      .SelectSingleNode("xs:element").Attributes.GetNamedItem("type").Value; 
+                        outParams += param.Type + "/";
                     }
 
-                    elementNode = elementNode.NextSibling; // goes to the next elementNode
+                    string data = opName + ":" + inputParams + ":" + outParams;
+                    WsOpDetails.Add(data); // inserts data into the array 
                 }
-
-                WsOpDetails[opIndex] = returnType + "|" + opName + "|" + paramTypes; // inserts data into the array 
-                opIndex++; 
             }
 
-            return WsOpDetails;
+            return WsOpDetails.ToArray();
+        }
+
+        private static Parameter[] GetParameters(ServiceDescription serviceDescription, string messagePartName)
+        {
+            List<Parameter> parameters = new List<Parameter>();
+
+            Types types = serviceDescription.Types;
+            XmlSchema xmlSchema = types.Schemas[0];
+
+            foreach (object item in xmlSchema.Items)
+            {
+                XmlSchemaElement schemaElement = item as System.Xml.Schema.XmlSchemaElement;
+                if (schemaElement != null)
+                {
+                    if (schemaElement.Name == messagePartName)
+                    {
+                        XmlSchemaType schemaType = schemaElement.SchemaType;
+                        XmlSchemaComplexType complexType = schemaType as System.Xml.Schema.XmlSchemaComplexType;
+                        if (complexType != null)
+                        {
+                            XmlSchemaParticle particle = complexType.Particle;
+                            XmlSchemaSequence sequence = particle as System.Xml.Schema.XmlSchemaSequence;
+                            if (sequence != null)
+                            {
+                                foreach (XmlSchemaElement childElement in sequence.Items)
+                                {
+                                    string parameterName = childElement.Name;
+                                    string parameterType = childElement.SchemaTypeName.Name;
+                                    parameters.Add(new Parameter(parameterName, parameterType));
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+            return parameters.ToArray();
+        }
+
+
+        public struct Parameter
+        {
+            /// <summary>
+            /// constructor
+            /// </summary>
+            /// <param name="name">
+            /// <param name="type">
+            public Parameter(string name, string type)
+            {
+                this.Name = name;
+                this.Type = type;
+            }
+
+            /// <summary>
+            /// Name
+            /// </summary>
+            public string Name;
+            /// <summary>
+            /// Type
+            /// </summary>
+            public string Type;
         }
     }
 }
