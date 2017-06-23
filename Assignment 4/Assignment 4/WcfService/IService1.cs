f66be41a0a97faa53234d71c492bb30cff28d58f
Harith Neralla <hneralla56@gmail.com>
2017-06-23 16:46:35 -0700

    added WCF service project

---
+++ Assignment 4/Assignment 4/WcfService/IService1.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+namespace WcfService
+{
+    [ServiceContract]
+    public interface IService1
+    {
+        [OperationContract]
+        string search(string url, string name);
+    }
+    
+}
---
+++ Assignment 4/Assignment 4/WcfService/Service1.svc.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+namespace WcfService
+{
+    public class Service1 : IService1
+    {
+        public string search(string url, string name)
+        {
+
+        }
+    }
+}
